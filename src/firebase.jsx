e88822c975a42e1f99180dbb13d696cb6728144c
VincentProfile <vincentccs93@gmail.com>
2021-12-11 20:47:35 +0800

    add Firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -11,13 +11,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6lLBw-GLGvdCRLAsse1tyYoQFjqsf4Gg",
-  authDomain: "resumewebapp-7e6da.firebaseapp.com",
-  projectId: "resumewebapp-7e6da",
-  storageBucket: "resumewebapp-7e6da.appspot.com",
-  messagingSenderId: "777774188472",
-  appId: "1:777774188472:web:1d620787bd4faf1ecff14d",
-  measurementId: "G-NT66TYGSB5"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
